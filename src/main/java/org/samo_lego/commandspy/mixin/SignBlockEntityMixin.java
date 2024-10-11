6ab007fcd241af097b5e373e1d6edb435ac9b627
Redlnn <w731347477@gmail.com>
2024-10-11 22:02:45 +0800

    Update to Minecraft 1.21.1

--- src/main/java/org/samo_lego/commandspy/mixin/SignBlockEntityMixin.java
+++ src/main/java/org/samo_lego/commandspy/mixin/SignBlockEntityMixin.java
@@ -12,7 +12,6 @@ import org.jetbrains.annotations.Nullable;
 import org.samo_lego.commandspy.CommandSpy;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
